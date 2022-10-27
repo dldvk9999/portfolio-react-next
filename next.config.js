c85e7816a3dd65bc275c6728b1cf369d7dadef4c
JongGeun <dldvk9999@gmail.com>
2022-10-28 07:40:29 +0900

    nextjs module patch

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
-import { join } from "path";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     sassOptions: {
-        includePaths: [join(__dirname, "styles")],
+        includePaths: [path.join(__dirname, "styles")],
         prependData: `@import "styles/_variables.scss"; @import "styles/_mixins.scss";`,
     },
 };
