603bc8f98c413fa54dadfb9335280a5b70c7fbc7
JongGeun <dldvk9999@gmail.com>
2022-10-31 19:04:01 +0900

    Home - scroll button patch

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
     const downArrow = () => {
         Scrollbar.get(
             document.querySelector("#root") as HTMLElement
-        )?.setMomentum(0, 1000);
+        )?.setMomentum(0, window.innerHeight);
     };
 
     useEffect(() => {
