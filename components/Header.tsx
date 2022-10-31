0ff1d24feea5e5c9a12531e589907ebc4eb9cb8d
JongGeun <dldvk9999@gmail.com>
2022-10-31 19:01:34 +0900

    Home - nav function & layout update

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useEffect } from "react";
+import Scrollbar from "smooth-scrollbar";
 import styles from "../styles/Home.module.scss";
 
 const Header = () => {
@@ -11,6 +12,12 @@ const Header = () => {
     function closeNav() {
         let nav = document.querySelector("#nav");
         nav?.classList.remove("active");
+    }
+
+    function navCloseButton(event: any) {
+        if (event.tagName === "svg") {
+            closeNav();
+        }
     }
 
     function routerList() {
@@ -33,41 +40,25 @@ const Header = () => {
     }
 
     useEffect(() => {
-        window.addEventListener("resize", () => {
-            if (screen.width > 600) {
+        window.onresize = () => {
+            if (window.innerWidth > 600) {
                 closeNav();
             }
-        });
+        };
+
+        document
+            .querySelector("#nav")!
+            .addEventListener("click", (e) => navCloseButton(e.target));
+
         return () => {
-            window.removeEventListener("resize", () => {
-                if (screen.width > 600) {
-                    closeNav();
-                }
-            });
+            document
+                .querySelector("#nav")!
+                .removeEventListener("click", (e) => navCloseButton(e.target));
         };
     }, []);
 
     return (
         <header className={styles.header}>
-            <nav id="nav">
-                <div>
-                    <div className={styles.navClose}>
-                        <button onClick={closeNav}>
-                            <svg
-                                viewBox="0 0 120.64 122.88"
-                                width="17"
-                                height="17"
-                                stroke="white"
-                                fill="white"
-                                xmlSpace="preserve"
-                            >
-                                <path d="M66.6,108.91c1.55,1.63,2.31,3.74,2.28,5.85c-0.03,2.11-0.84,4.2-2.44,5.79l-0.12,0.12c-1.58,1.5-3.6,2.23-5.61,2.2 c-2.01-0.03-4.02-0.82-5.55-2.37C37.5,102.85,20.03,84.9,2.48,67.11c-0.07-0.05-0.13-0.1-0.19-0.16C0.73,65.32-0.03,63.19,0,61.08 c0.03-2.11,0.85-4.21,2.45-5.8l0.27-0.26C20.21,37.47,37.65,19.87,55.17,2.36C56.71,0.82,58.7,0.03,60.71,0 c2.01-0.03,4.03,0.7,5.61,2.21l0.15,0.15c1.57,1.58,2.38,3.66,2.41,5.76c0.03,2.1-0.73,4.22-2.28,5.85L19.38,61.23L66.6,108.91 L66.6,108.91z M118.37,106.91c1.54,1.62,2.29,3.73,2.26,5.83c-0.03,2.11-0.84,4.2-2.44,5.79l-0.12,0.12 c-1.57,1.5-3.6,2.23-5.61,2.21c-2.01-0.03-4.02-0.82-5.55-2.37C89.63,101.2,71.76,84.2,54.24,67.12c-0.07-0.05-0.14-0.11-0.21-0.17 c-1.55-1.63-2.31-3.76-2.28-5.87c0.03-2.11,0.85-4.21,2.45-5.8C71.7,38.33,89.27,21.44,106.8,4.51l0.12-0.13 c1.53-1.54,3.53-2.32,5.54-2.35c2.01-0.03,4.03,0.7,5.61,2.21l0.15,0.15c1.57,1.58,2.38,3.66,2.41,5.76 c0.03,2.1-0.73,4.22-2.28,5.85L71.17,61.23L118.37,106.91L118.37,106.91z" />
-                            </svg>
-                        </button>
-                    </div>
-                    <div className={styles.navList}>{routerList()}</div>
-                </div>
-            </nav>
             <button className={styles.menu} onClick={openNav}>
                 <svg
                     fill="white"
@@ -79,7 +70,14 @@ const Header = () => {
                     <path d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z" />
                 </svg>
             </button>
-            <div className={styles.headerList}>{routerList()}</div>
+            <div className={styles.headerList}>
+                <div className={styles.headerRouters}>{routerList()}</div>
+                <div className={styles.headerAdmin}>
+                    <Link href="/admin">
+                        <a onClick={closeNav}>Admin</a>
+                    </Link>
+                </div>
+            </div>
         </header>
     );
 };
