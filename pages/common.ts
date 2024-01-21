11695018240686a478af8d4be258ddff5496b725
JongGeun <dldvk9999@naver.com>
2024-01-21 22:56:07 +0900

    Deploy - common 오류 수정

--- pages/common.ts
+++ pages/common.ts
@@ -8,7 +8,7 @@ export default function scrollUp() {
 
 // 최상단에 스크롤 시 플로팅 버튼 사라짐
 export function floatingHide(scrollTop: number) {
-    if (document) {
+    if (typeof document !== "undefined") {
         const floating = document.querySelector("#floating") as HTMLElement;
         if (scrollTop === 0 || window.innerHeight * 0.9 <= 460) {
             floating.classList.remove("floating-show");
@@ -25,21 +25,21 @@ export function setScreenSize() {
     if (scroll!.size.content.height < vh + scroll!.scrollTop) {
         scroll?.setMomentum(0, scroll!.size.content.height - vh);
     }
-    if (document) {
+    if (typeof document !== "undefined") {
         document.documentElement.style.setProperty("--vh", `${vh}px`);
     }
 }
 
 // ScrollBar 초기화
 export function scrollbarInit() {
-    if (document) {
+    if (typeof document !== "undefined") {
         Scrollbar.init(document.querySelector("#root") as HTMLElement);
     }
 }
 
 // ScrollBar return
 export function getScrollbar() {
-    if (document) {
+    if (typeof document !== "undefined") {
         return Scrollbar.get(document.querySelector("#root") as HTMLElement);
     } else {
         return null;
@@ -47,7 +47,7 @@ export function getScrollbar() {
 }
 
 export function hideTitle() {
-    if (document) {
+    if (typeof document !== "undefined") {
         // 각 페이지에 최초로 출력되는 타이틀 자동으로 숨겨지게 처리
         setTimeout(() => {
             const pageTitle = document.querySelector("#pageTitle") as HTMLElement;
